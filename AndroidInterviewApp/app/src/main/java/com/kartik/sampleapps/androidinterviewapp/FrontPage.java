ce88294462234c2a948e463cb7cedb17555fdf5c
Kartik Patel <kartiknasit@gmail.com>
2016-09-19 21:51:31 +0530

    This is the initial commit for Android Interview Application. This initial Commit contains the front page for the Application for which i have created different layout for different screen. Also to hightlight the Button Click i have added front_page_background_button xml in the drawable folder.

---
+++ AndroidInterviewApp/app/src/main/java/com/kartik/sampleapps/androidinterviewapp/FrontPage.java
@@ -0,0 +1,13 @@
+package com.kartik.sampleapps.androidinterviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrontPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_page);
+    }
+}
